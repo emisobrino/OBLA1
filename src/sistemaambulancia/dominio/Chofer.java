6867f9bfa0c0debec8b3887561b1441d7cdb56d3
guzmy <guzmy@172.29.1.192>
2017-06-19 21:11:13 -0300

    Merge origin/OBLA1 into OBLA1
    Conflicts:
    	src/sistemaambulancia/SistemaAmbulancia.java

--- src/sistemaambulancia/dominio/Chofer.java
+++ src/sistemaambulancia/dominio/Chofer.java
@@ -1,42 +1,46 @@
-
 package sistemaambulancia.dominio;
 
-
-public class Chofer {
+public class Chofer 
+{
+    //Properties
     private String cedula;
     private String nombre;
     private String idAmbulancia;
 
+    //Constructor
     public Chofer(String cedula, String nombre, String idAmbulancia) {
         this.cedula = cedula;
         this.nombre = nombre;
         this.idAmbulancia= idAmbulancia;
-                
     }
 
-    
-    
+    //Obtener cedula
     public String getCedula() {
         return cedula;
     }
 
+    //Setear cedula
     public void setCedula(String cedula) {
         this.cedula = cedula;
     }
 
+    //Obtener nombre
     public String getNombre() {
         return nombre;
     }
 
+    //Setear nombre
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
     
+    //Obtener id ambulancia en la que trabaja
     public String getIdAmbulancia(){
         return idAmbulancia;
     }
+    
+    //Setear id ambulancia a la que trabaja
     public void setIdAmbulancia(String idAmbulancia){
         this.idAmbulancia=idAmbulancia;
     }
-    
 }
