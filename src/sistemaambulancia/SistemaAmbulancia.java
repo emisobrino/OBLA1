36bfd2bc7281e78a081b35671b053f9039558c75
guzmy <guzmy@172.29.1.192>
2017-06-20 20:12:31 -0300

    eliminar ambulancia y chofer ruta mas cercana mas

--- src/sistemaambulancia/SistemaAmbulancia.java
+++ src/sistemaambulancia/SistemaAmbulancia.java
@@ -15,10 +15,9 @@ public class SistemaAmbulancia implements ISistema {
     private ListaCiudad listaCiudades;
 
     @Override
-    public TipoRet crearSistemaDeEmergencias(int cantidadCiudades) 
-    {
+    public TipoRet crearSistemaDeEmergencias(int cantidadCiudades) {
         listaCiudades = new ListaCiudad(cantidadCiudades);
-        
+
         if (cantidadCiudades <= 0) {
             System.out.println("La cantidad de ciudades es inferior a 1.");
             return TipoRet.ERROR;
@@ -45,11 +44,10 @@ public class SistemaAmbulancia implements ISistema {
         //Si no esta vacia entonces encontro la ambulancia
         if (ambu != null) {
             //Si no esta en estado de emergencia entonces borro la ambulancia
-            if(ambu.getEstado() != Ambulancia.TipoEstado.ATENDIENDO_EMERGENCIA)
-            {
+            if (ambu.getEstado() != Ambulancia.TipoEstado.ATENDIENDO_EMERGENCIA) {
                 listaAmbulancias.eliminarAmbulancia(ambulanciaID);
                 return TipoRet.OK;
-            }else{
+            } else {
                 System.out.println("No es posible eliminar la ambulancia ambulanciaID");
                 return TipoRet.ERROR;
             }
@@ -162,13 +160,10 @@ public class SistemaAmbulancia implements ISistema {
     @Override
     public TipoRet informeAmbulancia() {
         //Si la lista de ambulancias no esta vacia entonces llamo a mostrarAmbulancias
-        if (!listaAmbulancias.esVacia()) 
-        {
+        if (!listaAmbulancias.esVacia()) {
             System.out.println("Listado de ambulancias:");
             listaAmbulancias.mostrarAmbulancias();
-        } 
-        else 
-        {
+        } else {
             System.out.println("No se han ingresado ambulancias");
         }
 
@@ -192,27 +187,24 @@ public class SistemaAmbulancia implements ISistema {
     }
 
     @Override
-    public TipoRet cambiarUbicacion(String ambulanciaID, int ciudadID) 
-    {
+    public TipoRet cambiarUbicacion(String ambulanciaID, int ciudadID) {
         //Busco ambulancia y ciudad
         Ambulancia ambu = listaAmbulancias.buscar(ambulanciaID);
         Ciudad ciuNueva = listaCiudades.buscar(ciudadID);
         //Guardo el valor de la ciudad de donde parte la ambulancia
         Ciudad ciuVieja = ambu.getCiudad();
-        
-        if(ambu != null)
-        {
-            if(ciuNueva != null)
-            {
+
+        if (ambu != null) {
+            if (ciuNueva != null) {
                 ambu.setCiudad(ciuNueva);   //SETEO CIUDAD DE DESTINO
                 ciuNueva.getAmbulancias().insertarOrdenado(ambu);  //A LA CIUDAD DE DESTINO LE AGREGO LA AMBULANCIA
                 ciuVieja.getAmbulancias().eliminarAmbulancia(ambulanciaID);   //A LA CIUDAD VIEJA LE QUITO LA AMBULANCIA
                 return TipoRet.OK;
-            }else{
+            } else {
                 System.out.println("La ciudad ciudadID no existe");
                 return TipoRet.ERROR;
             }
-        }else{
+        } else {
             System.out.println("No existe una ambulancia con identificador ambulanciaID");
             return TipoRet.ERROR;
         }
@@ -258,81 +250,110 @@ public class SistemaAmbulancia implements ISistema {
 
     @Override
     public TipoRet agregarRuta(int ciudadOrigen, int ciudadDestino, int minutosViaje) {
-        
-       //se fija si las ciudades con sus ids existen en la lista de ciudades y que la duracion sea mayor a 0
+
+        //se fija si las ciudades con sus ids existen en la lista de ciudades y que la duracion sea mayor a 0
         if (this.listaCiudades.buscar(ciudadOrigen) == null) {
             System.out.println("La ciudad " + ciudadOrigen + " no existe.");
             return TipoRet.ERROR;
-        }else if(this.listaCiudades.buscar(ciudadDestino)==null){
+        } else if (this.listaCiudades.buscar(ciudadDestino) == null) {
             System.out.println("La ciudad " + ciudadDestino + " no existe.");
             return TipoRet.ERROR;
-        }else if(minutosViaje<=0){
+        } else if (minutosViaje <= 0) {
             System.out.println("La duración del viaje debe ser mayor que 0.");
             return TipoRet.ERROR;
-        // comprueba que la ciudad no sea la misma    
-        }else if (ciudadDestino == ciudadOrigen) {
+            // comprueba que la ciudad no sea la misma    
+        } else if (ciudadDestino == ciudadOrigen) {
             System.out.println("La ciudad Origen es igual a la ciudad Destino");
             return TipoRet.ERROR;
-        }else{
-        //existen las 2 ciudades y la duracion es mayor a 0
-            mapa[ciudadOrigen][ciudadDestino]=minutosViaje;
-            mapa[ciudadDestino][ciudadOrigen]=minutosViaje;
+        } else {
+            //existen las 2 ciudades y la duracion es mayor a 0
+            mapa[ciudadOrigen][ciudadDestino] = minutosViaje;
+            mapa[ciudadDestino][ciudadOrigen] = minutosViaje;
             return TipoRet.OK;
         }
     }
 
     @Override
     public TipoRet modificarDemora(int ciudadOrigen, int ciudadDestino, int minutosViaje) {
-        
+
         //se fija si las ciudades con sus ids existen en la lista de ciudades y que la duracion sea mayor a 0
         if (this.listaCiudades.buscar(ciudadOrigen) == null) {
             System.out.println("La ciudad " + ciudadOrigen + " no existe.");
             return TipoRet.ERROR;
-        }else if(this.listaCiudades.buscar(ciudadDestino)==null){
+        } else if (this.listaCiudades.buscar(ciudadDestino) == null) {
             System.out.println("La ciudad " + ciudadDestino + " no existe.");
             return TipoRet.ERROR;
-        }else if(minutosViaje<=0){
+        } else if (minutosViaje <= 0) {
             System.out.println("La duración del viaje debe ser mayor que 0.");
             return TipoRet.ERROR;
-        // comprueba que la ciudad no sea la misma    
-        }else if (ciudadDestino == ciudadOrigen) {
+            // comprueba que la ciudad no sea la misma    
+        } else if (ciudadDestino == ciudadOrigen) {
             System.out.println("La ciudad Origen es igual a la ciudad Destino");
             return TipoRet.ERROR;
-        }else{
+        } else {
             //existen las 2 ciudades y la duracion es mayor a 0
             //se fija si la posicion a modificar nunca fue agregada,
             //si es -1, es decir que nunca se agrego llama a agregar ruta, sino la agrega.
-            if (mapa[ciudadOrigen][ciudadDestino]== -1) {
+            if (mapa[ciudadOrigen][ciudadDestino] == -1) {
                 agregarRuta(ciudadOrigen, ciudadDestino, minutosViaje);
                 return TipoRet.OK;
-            }else{
-                mapa[ciudadOrigen][ciudadDestino]=minutosViaje;
-                mapa[ciudadDestino][ciudadOrigen]=minutosViaje;
+            } else {
+                mapa[ciudadOrigen][ciudadDestino] = minutosViaje;
+                mapa[ciudadDestino][ciudadOrigen] = minutosViaje;
                 return TipoRet.OK;
-                }
+            }
         }
     }
 
     @Override
     public TipoRet ambulanciaMasCercana(int ciudadID) {
-        
-        if (this.listaCiudades.buscar(ciudadID)!=null) {
+
+        if (this.listaCiudades.buscar(ciudadID) != null) {
             //terminar
-        }else{
-            System.out.println("La ciudad "+ciudadID+" no existe.");
+        } else {
+            System.out.println("La ciudad " + ciudadID + " no existe.");
         }
-        
+
         return TipoRet.NO_IMPLEMENTADA;
     }
 
     @Override
     public TipoRet rutaMasRapida(int ciudadOrigen, int ciudadDestino) {
-        return TipoRet.NO_IMPLEMENTADA;
+        //Busco ciudades
+        Ciudad ciuOrigen = listaCiudades.buscar(ciudadOrigen);
+        Ciudad ciuDestino = listaCiudades.buscar(ciudadDestino);
+
+        if (ciuOrigen != null) {
+            if (ciuDestino != null) {
+                int camino = 0;
+                int columnas = mapa[0].length;
+                int columna = 0;
+                int suma = 0;
+                int minimo = 9999;
+
+                for (int i = 0; i < columnas; i++) {
+                    if (mapa[ciudadOrigen][i] != 0 && mapa[ciudadDestino][i] != 0) {
+                        suma = mapa[ciudadOrigen][i] + mapa[ciudadDestino][i];
+                        if (suma < minimo) {
+                            minimo = suma;
+                            columna = i;
+                        }
+                    }
+                }
+                return TipoRet.OK;
+            } else {
+                System.out.println("La ciudad ciudadDestino no existe");
+                return TipoRet.ERROR;
+            }
+        } else {
+            System.out.println("La ciudad ciudadOrigen no existe");
+            return TipoRet.ERROR;
+        }
     }
 
     @Override
     public TipoRet informeCiudades() {
-        
+
         return TipoRet.NO_IMPLEMENTADA;
     }
 
@@ -342,71 +363,64 @@ public class SistemaAmbulancia implements ISistema {
     }
 
     @Override
-    public TipoRet registrarChofer(String ambulanciaID, String nombre, String cedula) 
-    {
+    public TipoRet registrarChofer(String ambulanciaID, String nombre, String cedula) {
         //Busco ambulancia
         Ambulancia ambu = listaAmbulancias.buscar(ambulanciaID);
-        
+
         //Si la ambulancia no esta vacia entonces encontro la ambulancia
-        if(ambu != null)
-        {
+        if (ambu != null) {
             //Si no existe ya un chofer con esa cedula 
-            if(!listaChoferes.contains(cedula))
-            {
+            if (!listaChoferes.contains(cedula)) {
                 //Creo chofer y lo agrego a la lista
                 Chofer chofer = new Chofer(cedula, nombre, ambulanciaID);
                 listaChoferes.insertarOrdenado(chofer);
                 return TipoRet.OK;
-            }else{
+            } else {
                 System.out.println("Ya existe un chofer con esa cedula habilitado para conducir una ambulancia");
                 return TipoRet.ERROR;
             }
-        }else{
+        } else {
             System.out.println("No existe una ambulancia con identificador ambulanciaID");
             return TipoRet.ERROR;
         }
     }
 
     @Override
-    public TipoRet eliminarChofer(String ambulanciaID, String cedula) 
-    {
+    public TipoRet eliminarChofer(String ambulanciaID, String cedula) {
         //Busco ambulancia
         Ambulancia ambu = listaAmbulancias.buscar(ambulanciaID);
         //Busco chofer
         Chofer chofer = listaChoferes.buscar(cedula);
-        
+
         //Si la ambulancia no esta vacia entonces encontro la ambulancia
-        if(ambu != null)
-        {
-            if(chofer != null){
+        if (ambu != null) {
+            if (chofer != null) {
                 //Lo elimino de la lista de choferes haiblitados para esta ambulancia
                 ambu.getChoferes().eliminarChofer(cedula);
                 return TipoRet.OK;
-            }else{
+            } else {
                 System.out.println("No existe ese chofer con esa cedula");
                 return TipoRet.ERROR;
             }
-        }else{
+        } else {
             System.out.println("No existe una ambulancia con identificador ambulanciaID");
             return TipoRet.ERROR;
         }
     }
 
     @Override
-    public TipoRet informeChoferes(String ambulanciaID) 
-    {
+    public TipoRet informeChoferes(String ambulanciaID) {
         //Busco ambulancia
         Ambulancia ambu = listaAmbulancias.buscar(ambulanciaID);
-        
+
         //Si la ambulancia no esta vacia entonces encontro la ambulancia
-        if(ambu != null)
-        {
+        if (ambu != null) {
             System.out.println("Informe choferes de <" + ambulanciaID + ">");
-            
+
             //Muestro lista de choferes habilitados en esta ambulancia 
             ambu.getChoferes().mostrarChoferes();
             return TipoRet.OK;
-        }else{
+        } else {
             System.out.println("No existe una ambulancia con identificador ambulanciaID");
             return TipoRet.ERROR;
         }
